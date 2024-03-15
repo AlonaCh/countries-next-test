b478a22c1028f75eb06a14a204287a7106bbad52
Alona Chubenko <aliona.chubenko@gmail.com>
2024-03-15 13:10:18 +0200

    Cypress implementation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, jest: true },
+  env: { browser: true, es2020: true, jest: true, "cypres/globals": true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
